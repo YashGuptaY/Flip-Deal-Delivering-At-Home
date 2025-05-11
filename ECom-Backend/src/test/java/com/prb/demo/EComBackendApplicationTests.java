aa8a2d005a811f60ef1bffb70a101696774e5837
YashGuptaY <140195377+YashGuptaY@users.noreply.github.com>
2025-05-11 23:38:08 +0530

    Add files via upload

---
+++ ECom-Backend/src/test/java/com/prb/demo/EComBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prb.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EComBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
