7263dad6d1fc69485724ad193934535882830246
Yash Gupta <guptaygyash@gmail.com>
2025-05-24 16:19:27 +0530

    added missing files

---
+++ ECom-Backend/src/main/java/com/prb/demo/io/ProductUpdateReq.java
@@ -0,0 +1,17 @@
+package com.prb.demo.io;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProductUpdateReq {
+	private String id;
+    private String name;
+    private String description;
+    private double price;
+}
